67e192a86bf2cc5b998cd366ef320edb97232d2b
Saraeidem <saeid8969@usn.no>
2025-08-25 14:34:08 +0200

    Update oppgave-2.php

--- oppgave-2.php
+++ oppgave-2.php
@@ -3,10 +3,11 @@
 /* Programmet mottar fra et HTML-skjema et navn og en alder ved POST-metoden
 /* Programmet skriver ut en "god dag"-melding med personens navn og alder
 */
-<p><a href="index2.html">Tilbake til skjema</a></p>
+<p><a href="index.html">Tilbake til skjema</a></p>
 $fornavn=$_POST ["fornavn"];
 $etternavn=$_POST ["etternavn"];
 $alder=$_POST ["alder"]; /* variable gitt verdier fra feltene i HTML-skjemaet */
 print ("God dag $fornavn $etternavn, du er $alder &aring;r og like sprek. <br />"); /* resultat skrevet ut */
 ?>
+
 
