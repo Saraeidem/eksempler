a6f1850bbf44376ba35a91499daf27d78b8f3acf
Saraeidem <saeid8969@usn.no>
2025-08-25 12:57:34 +0200

    Update eksempel-1.php

--- eksempel-1.php
+++ eksempel-1.php
@@ -1,10 +1,25 @@
-<?php    /* Eksempel 1 */
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet skriver ut en "god dag"-melding med personens navn 
-*/
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
-	
-  print ("God dag $fornavn $etternavn <br />");  
+<?php
+// Viser feil i nettleseren (nyttig for debugging)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Hent verdier fra skjemaet (POST)
+$fornavn   = htmlspecialchars($_POST['fornavn'] ?? '');
+$etternavn = htmlspecialchars($_POST['etternavn'] ?? '');
 ?>
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <meta charset="UTF-8">
+  <title>God dag</title>
+</head>
+<body>
+  <?php if ($fornavn && $etternavn): ?>
+    <p>God dag <?php echo $fornavn . ' ' . $etternavn; ?></p>
+  <?php else: ?>
+    <p>Du må fylle ut både fornavn og etternavn.</p>
+  <?php endif; ?>
+
+  <p><a href="../../html.eksempel1">Tilbake til skjema</a></p>
+</body>
+</html>
