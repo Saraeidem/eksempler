c3ee560f730c06f998c519dcce9376db48dc557e
Saraeidem <saeid8969@usn.no>
2025-08-25 14:01:22 +0200

    Update eksempel-2.php

---
+++ eksempel-2.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$fornavn   = $_POST['fornavn']   ?? '';
+$etternavn = $_POST['etternavn'] ?? '';
+?>
+<!DOCTYPE html>
+<html lang="no">
+<head><meta charset="UTF-8"><title>God dag</title></head>
+<body>
+  <?php if ($fornavn && $etternavn): ?>
+    <p>God dag <?php echo htmlspecialchars($fornavn).' '.htmlspecialchars($etternavn); ?></p>
+  <?php else: ?>
+    <p>Du må fylle ut både fornavn og etternavn.</p>
+  <?php endif; ?>
+  <p><a href="index.html">Tilbake</a></p>
+</body>
+</html>
