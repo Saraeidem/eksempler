3e043434f31eed3e6db78529490fbadd6fef88b7
Saraeidem <saeid8969@usn.no>
2025-08-26 10:13:52 +0200

    endring

--- tema01/0104.php
+++ tema01/0104.php
@@ -1,24 +1,17 @@
-<?php   /* Eksempel 4 */
-/*
-    Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
-    Programmet skriver ut de 2 tallene og summen, differansen, produktet og kvotienten
-*/
-
+<?php  /* Eksempel 4 */
 $tall1 = $_POST["tall1"];
-$tall2 = $_POST["tall2"];   /* variablene får verdier fra feltene i HTML-skjemaet */
+$tall2 = $_POST["tall2"];
 
-$sum = $tall1 + $tall2;           /* beregning av summen utført */
-$differanse = $tall1 - $tall2;    /* beregning av differansen utført */
-$produkt = $tall1 * $tall2;       /* beregning av produktet utført */
+$sum        = $tall1 + $tall2;
+$differanse = $tall1 - $tall2;
+$produkt    = $tall1 * $tall2;
 
-/* kvotient – håndter deling på null */
 if ($tall2 == 0) {
     $kvotient = "Udefinert (deling på null)";
 } else {
     $kvotient = $tall1 / $tall2;
 }
 
-/* utskrift av resultater */
 print ("Tall 1 er $tall1 <br />");
 print ("Tall 2 er $tall2 <br />");
 print ("<br />");
@@ -26,4 +19,3 @@ print ("Summen er $sum <br />");
 print ("Differansen er $differanse <br />");
 print ("Produktet er $produkt <br />");
 print ("Kvotienten er $kvotient <br />");
-?>
