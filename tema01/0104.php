db421d34a01ad096b096f795b557925cc147f721
Saraeidem <saeid8969@usn.no>
2025-08-26 10:32:39 +0200

    endring i oppg

--- tema01/0104.php
+++ tema01/0104.php
@@ -1,21 +1,38 @@
-<?php  /* Eksempel 4 */
-$tall1 = $_POST["tall1"];
-$tall2 = $_POST["tall2"];
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$sum        = $tall1 + $tall2;
-$differanse = $tall1 - $tall2;
-$produkt    = $tall1 * $tall2;
+echo "<pre style='background:#f7f7f7;padding:10px;border:1px solid #ddd'>";
+echo "REQUEST_METHOD: " . ($_SERVER['REQUEST_METHOD'] ?? 'ukjent') . "\n";
+echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? 'ukjent') . "\n";
+echo "_POST:\n";
+var_dump($_POST);
+echo "</pre><hr>";
 
-if ($tall2 == 0) {
-    $kvotient = "Udefinert (deling på null)";
-} else {
-    $kvotient = $tall1 / $tall2;
+if (($_SERVER['REQUEST_METHOD'] ?? '') !== 'POST') {
+    echo 'Forventet POST fra 0104.html. ';
+    echo '<a href="0104.html">Gå til skjema</a>';
+    exit;
 }
 
-print ("Tall 1 er $tall1 <br />");
-print ("Tall 2 er $tall2 <br />");
-print ("<br />");
-print ("Summen er $sum <br />");
-print ("Differansen er $differanse <br />");
-print ("Produktet er $produkt <br />");
-print ("Kvotienten er $kvotient <br />");
+if (!isset($_POST['tall1'], $_POST['tall2'])) {
+    echo "Mangler felt (tall1/tall2) i POST. Sjekk name-attributtene i HTML.";
+    echo '<br><a href="0104.html">Tilbake</a>';
+    exit;
+}
+
+$t1 = (float)$_POST['tall1'];
+$t2 = (float)$_POST['tall2'];
+
+$sum        = $t1 + $t2;
+$differanse = $t1 - $t2;
+$produkt    = $t1 * $t2;
+$kvotient   = ($t2 == 0.0) ? 'Udefinert (deling på null)' : ($t1 / $t2);
+
+echo "Tall 1 er " . htmlspecialchars($_POST['tall1']) . "<br>";
+echo "Tall 2 er " . htmlspecialchars($_POST['tall2']) . "<br><br>";
+echo "Summen er $sum<br>";
+echo "Differansen er $differanse<br>";
+echo "Produktet er $produkt<br>";
+echo "Kvotienten er $kvotient<br><br>";
+echo '<a href="0104.html">Tilbake til skjemaet</a>';
