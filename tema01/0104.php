f80a9bd80c978965498ee11b1f2333b3b34aedaf
Saraeidem <saeid8969@usn.no>
2025-08-26 10:05:25 +0200

    endirng

--- tema01/0104.php
+++ tema01/0104.php
@@ -1,33 +1,29 @@
-<?php
-// Skru på feilmeldinger (nyttig under utvikling)
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+<?php   /* Eksempel 4 */
+/*
+    Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
+    Programmet skriver ut de 2 tallene og summen, differansen, produktet og kvotienten
+*/
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $tall1 = (float)($_POST['tall1'] ?? 0);
-    $tall2 = (float)($_POST['tall2'] ?? 0);
+$tall1 = $_POST["tall1"];
+$tall2 = $_POST["tall2"];   /* variablene får verdier fra feltene i HTML-skjemaet */
 
-    $sum = $tall1 + $tall2;
-    $differanse = $tall1 - $tall2;
-    $produkt = $tall1 * $tall2;
+$sum = $tall1 + $tall2;           /* beregning av summen utført */
+$differanse = $tall1 - $tall2;    /* beregning av differansen utført */
+$produkt = $tall1 * $tall2;       /* beregning av produktet utført */
 
-    // Pass på deling på null
-    if ($tall2 != 0) {
-        $kvotient = $tall1 / $tall2;
-    } else {
-        $kvotient = "Udefinert (deling på null)";
-    }
-
-    echo "Tall 1 er $tall1<br>";
-    echo "Tall 2 er $tall2<br>";
-    echo "Summen er $sum<br>";
-    echo "Differansen er $differanse<br>";
-    echo "Produktet er $produkt<br>";
-    echo "Kvotienten er $kvotient<br><br>";
-
-    echo '<a href="0104.html">Tilbake til skjemaet</a>';
+/* kvotient – håndter deling på null */
+if ($tall2 == 0) {
+    $kvotient = "Udefinert (deling på null)";
 } else {
-    header('Location: 0104.html');
-    exit;
+    $kvotient = $tall1 / $tall2;
 }
+
+/* utskrift av resultater */
+print ("Tall 1 er $tall1 <br />");
+print ("Tall 2 er $tall2 <br />");
+print ("<br />");
+print ("Summen er $sum <br />");
+print ("Differansen er $differanse <br />");
+print ("Produktet er $produkt <br />");
+print ("Kvotienten er $kvotient <br />");
 ?>
